109a757bd3429bba35a4ff50b449d261d5b35bfe
Brayan Gomez <sacmaiki108@gmail.com>
2023-02-14 15:06:08 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 print "Brayan Eduardo Gomez Lopez 1651723"
+import os
+mi ubicacion = os.getcwd()
+if os.path.exists("modulos"):
+    print("la carpeta ya existe")
+    else:
+         os.mkdir(mi ubicacion + "\\modulos")
+         archivo = open('./modulos/prueba.txt','w' )
+         archivo.write('hola mundo')
+         archivo.close()
